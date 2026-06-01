bff1954172c40003cf44d5899b47447b342c9ba2
d <david.vakhayev@gmail.com>
2026-06-01 16:51:44 +0200

    f

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/FE23-js3-inlamningsuppgift-2-david-vakhayev/",
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+    open: true,
+  },
+});
